1842d82dd77b4dcbc110bdd9beda334c411cc880
akadeepesh <deepesh.kumar.ug21@nsut.ac.in>
2023-07-09 02:28:47 +0530

    Add files via upload

---
+++ members/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Member
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "joined_date",)
+
+admin.site.register(Member, MemberAdmin)
+
+# Register your models here.
---
+++ members/views.py
@@ -0,0 +1,35 @@
+# Code Till GOAT.
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {              
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context,request))
+
+def main(request):
+    var = Member.objects.all().values()
+    template = loader.get_template('main.html')
+    context = {
+        'var' : var
+    }
+    return HttpResponse(template.render(context,request))
+
+def testing(request):
+    template = loader.get_template('template.html')
+    context = {
+        'fruits' : ['Apple', 'Banana', 'Cherry'],
+    }
+    return HttpResponse(template.render(context, request))
